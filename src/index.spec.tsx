12f6f870192a75779f100b3c127c2006ec325d51
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-05-29 16:23:51 +0200

    Minor formatting

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -2,7 +2,7 @@ import { describe, expect, it, vi } from 'vitest';
 import { createRef } from 'react';
 import { render } from '@testing-library/react';
 
-import mergeRefs from './index';
+import mergeRefs from './index.js';
 
 describe('mergeRefs()', () => {
   it('returns falsy result given no arguments', () => {
